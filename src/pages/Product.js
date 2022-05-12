27677f8e0348f91580dedc1d193ac9832b828007
yryskul <kuttubaevayryskul@gmail.com>
2022-05-12 20:44:12 +0600

    added components

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import Header from "../components/Header/Header";
-import { getProduct } from "../data/products";
+import {getProduct} from '../data/products'
 
 function Product() {
   const params = useParams();
