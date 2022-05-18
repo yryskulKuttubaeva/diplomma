fd9d99850233270ae71c7cfb8ddd45588d84c7f2
yryskul <kuttubaevayryskul@gmail.com>
2022-05-18 14:00:01 +0600

    changes com

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Cart from "./Components/Cart";
-import Nav from "./Components/Nav";
-import Header from "./Components/Header";
-import HomeScreen from "./screens/HomeScreen";
-import Footer from "./Components/Footer";
+
 
 function App() {
   return (
     <div className='App'>
-      <Nav />
-      <Header/>
-      <Cart />
-      <HomeScreen />
-      <Footer/>
+      
     </div>
   );
 }
