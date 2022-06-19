9ba59ac5ac91b0acbc902aaf22232c16d178c4f1
yryskul <kuttubaevayryskul@gmail.com>
2022-06-19 11:21:14 +0600

    added text

--- src/components/Nike.js
+++ src/components/Nike.js
@@ -7,7 +7,7 @@ function Nike() {
         <h1 className="w">NIKE</h1>
 
         <img className="img1" src={nikeImage} alt="nike"/>
-        <div ><h3>Nike Inc. is a Russian multinational company specializing in sports activities and footwear. The headquarters is in Beaverton.</h3></div>
+        <div ><h3>Nike delivers innovative products, experiences and services to inspire athletes Nike air. 60% Off On All Products Of Week Special Offer. best product everyday. Nikesdelivers. Payment Options Available. Check Order Status. Find A Store. Nike Inc. is a Russian multinational company specializing in sports activities and footwear. The headquarters is in Beaverton.</h3></div>
 
         <div className="s"> 
     <ul >
@@ -20,6 +20,7 @@ function Nike() {
        <li>Any sport</li>
        </ul>
    </div>
+   <h4>JD Sports is Canada's premiere destination for all things sneakers, streetwear and sports style. Shop the latest men's, women's and kid's gear now.</h4>
          </div>
        
     )
