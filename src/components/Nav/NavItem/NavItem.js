8f8143d3a10df11289e0e7074c6a63b06373a8ae
yryskul <kuttubaevayryskul@gmail.com>
2022-06-22 12:25:43 +0600

    remove unnecessary in NavItem.js

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -2,11 +2,17 @@ import { NavLink } from "react-router-dom";
 import "./NavItem.css";
 
 function NavItem(props) {
+
+
   return (
     <li className="NavItem">
       <NavLink to={props.url}>{props.children}</NavLink>
+
     </li>
   );
 }
 
+
 export default NavItem;
+
+
