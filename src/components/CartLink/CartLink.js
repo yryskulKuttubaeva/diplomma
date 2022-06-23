dd415fdd766227ad07f0761e706f7a650f507184
yryskul <kuttubaevayryskul@gmail.com>
2022-06-23 13:53:17 +0600

    import CartLink.css

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -1,12 +1,18 @@
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import cartImage from "../../assets/cart-icons.svg"
+import "./CartLink.css"
 
 export default function CartLink() {
-  const number = useSelector(store => {
+  const number = useSelector (store => {
     return Object.values(store.cart.items).reduce((sum, number) => sum + number, 0);
   });
 
   return (
-    <NavLink to="/cart">Cart ({number})</NavLink>
+    <NavLink to="/cart">
+              <div className="number">{number}</div>
+
+            <img className="cart-icons" src={cartImage} alt="svg" />
+      </NavLink>
   );
 }
