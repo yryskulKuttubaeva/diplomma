6a25cfd9e45b5a7f3ea79b92b584f1273780851c
yryskul <kuttubaevayryskul@gmail.com>
2022-06-23 13:54:54 +0600

    import module.css in NavItem

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -1,18 +1,16 @@
-import { NavLink } from "react-router-dom";
-import "./NavItem.css";
+import { NavLink, useMatch } from "react-router-dom";
+import classes from "./NavItem.module.css";
 
 function NavItem(props) {
-
-
   return (
-    <li className="NavItem">
-      <NavLink to={props.url}>{props.children}</NavLink>
-
+    <li className={classes.NavItem}>
+      <NavLink to={props.url}
+        className={useMatch(props.url) ? classes.active : null}
+      >
+        {props.children}
+      </NavLink>
     </li>
   );
 }
 
-
 export default NavItem;
-
-
