6d040739e7735de4f5b9eff68f6239f7e5cd1d68
yryskul <kuttubaevayryskul@gmail.com>
2022-06-23 13:51:31 +0600

    add dispatch and useEffect in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import Home from "./pages/Home";
-
+import Contacts from "./pages/Contacts";
+import Products from "./pages/Products";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Product from "./pages/Product";
 import NotFound from "./pages/NotFound";
-import Products from "./pages/Products";
-import Contacts from "./pages/Contacts";
 import Categories from "./pages/Categories";
 import Category from "./pages/Category";
+import Cart from "./pages/Cart";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch({ type: 'cart/restore' });
+  }, []);
+
   return (
     <div className="App">
       <Layout>
@@ -17,11 +25,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:productId" element={<Product />} />
-
           <Route path="/categories" element={<Categories />} />
           <Route path="/categories/:categoryId" element={<Category />} />
-
           <Route path="/contacts" element={<Contacts />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
