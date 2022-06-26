337d91fcbd11c15f0f82af6454402705d7621b46
yryskul <kuttubaevayryskul@gmail.com>
2022-06-26 23:25:55 +0600

    delete useDispatch and useEssect for App.js

--- src/App.js
+++ src/App.js
@@ -8,17 +8,10 @@ import NotFound from "./pages/NotFound";
 import Categories from "./pages/Categories";
 import Category from "./pages/Category";
 import Cart from "./pages/Cart";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
 import Checkout from "./pages/Checkout";
 import Auth from "./pages/Auth";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch({ type: "cart/restore" });
-  });
 
   return (
     <div className="App">
